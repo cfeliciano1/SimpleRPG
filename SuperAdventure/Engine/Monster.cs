9b1be90f3b0b4b086f948d470c4b961801fe966d
cfeliciano1 <cory.111@live.com>
2016-04-11 20:44:20 -0500

    Added tons of content
    Added backend classes, front end is showing but no logic yet

---
+++ SuperAdventure/Engine/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Monster : LivingCreature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
+
+        public Monster (int currentHitPoints, int maximumHitPoints, int id, string name, int maximumDamage, int rewardExperience, int rewardGold) : base(currentHitPoints, maximumHitPoints)
+        {
+            ID = id;
+            Name = name;
+            MaximumDamage = maximumDamage;
+            RewardExperiencePoints = rewardExperience;
+            RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
+        }
+    }
+}
--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -8,13 +8,55 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Engine;
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
+        private Player player1;
+
         public SuperAdventure()
         {
             InitializeComponent();
+
+            Location location = new Location(1, "Home", "This is your house.");
+
+            player1 = new Player(10, 10, 20, 0, 1);
+
+            lblHitPoints.Text = player1.CurrentHitPoints.ToString();
+            lblGold.Text = player1.Gold.ToString();
+            lblExperience.Text = player1.ExperiencePoints.ToString();
+            lblLevel.Text = player1.Level.ToString();
+        }
+
+        private void btnWest_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNorth_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEast_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSouth_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUseWeapon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUsePotion_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
